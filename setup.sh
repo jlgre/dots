f6ad6305c44b36800099c5c98be8ca69fa8bb876
Luke Green <lgreen@redhat.com>
2022-08-23 14:52:51 -0400

    fix script

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ ln -s "$(pwd)/bashrc" ~/.bashrc
 
 [ -d ~/.config/nvim ] || mkdir -p ~/.config/nvim
 [ -f ~/.config/nvim/init.lua ] && mv ~/.config/nvim/init.lua ~/.config/nvim/init.lua.bak
-ln -s "$(pwd)/init.lua" "~/.config/nvim/init.lua"
+ln -s "$(pwd)/init.lua" ~/.config/nvim/init.lua
