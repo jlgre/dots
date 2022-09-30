c971fc6248dc15d3426a58f761fbb94e85f0c6bf
Luke Green <lukeg496@gmail.com>
2022-09-30 13:58:35 -0500

    explicit completion bindings

--- init.lua
+++ init.lua
@@ -61,6 +61,15 @@ local lsp = require'lspconfig'
 -- Completion
 local cmp = require'cmp'
 
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
 cmp.setup({
 	snippet = {
 		expand = function(args)
@@ -70,8 +79,37 @@ cmp.setup({
 	mapping = {
 		['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
 		['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-		['C-Space'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-		['<CR>'] = cmp.mapping.confirm({ select = true })
+		['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
+		["<Tab>"] = cmp.mapping(function(fallback)
+		  if cmp.visible() then
+			cmp.select_next_item()
+		  elseif vim.fn["vsnip#available"](1) == 1 then
+			feedkey("<Plug>(vsnip-expand-or-jump)", "")
+		  elseif has_words_before() then
+			cmp.complete()
+		  else
+			fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+		  end
+		end, { "i", "s" }),		
+		["<Down>"] = cmp.mapping(function(fallback)
+		  if cmp.visible() then
+			cmp.select_next_item()
+		  elseif vim.fn["vsnip#available"](1) == 1 then
+			feedkey("<Plug>(vsnip-expand-or-jump)", "")
+		  elseif has_words_before() then
+			cmp.complete()
+		  else
+			fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+		  end
+		end, { "i", "s" }),		
+		["<Up>"] = cmp.mapping(function()
+		  if cmp.visible() then
+			cmp.select_prev_item()
+		  elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+			feedkey("<Plug>(vsnip-jump-prev)", "")
+		  end
+		end, { "i", "s" }),
 	},
 	sources = cmp.config.sources({
 		{ name = 'nvim_lsp' },
