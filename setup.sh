2141d7bd42b0c06a2a5f065a8a9c383962a1f0c8
Luke Green <lukeg496@gmail.com>
2022-10-27 14:42:46 -0500

    tmux conf

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 [ -f ~/.bashrc ] && mv ~/.bashrc ~/.bashrc.bak
 ln -s "$(pwd)/bashrc" ~/.bashrc
 
+[ -f ~/.tmux.conf ] && mv ~/.tmux.conf ~/.tmux.conf.bak
+ln -s "$(pwd)/tmux.conf" ~/.tmux.conf
+
 [ -d ~/.config/nvim ] || mkdir -p ~/.config/nvim
 [ -f ~/.config/nvim/init.lua ] && mv ~/.config/nvim/init.lua ~/.config/nvim/init.lua.bak
 ln -s "$(pwd)/init.lua" ~/.config/nvim/init.lua
