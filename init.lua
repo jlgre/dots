8f40f446b4859d3809bacf15cd2e7a224097d7df
Luke Green <lukeg496@gmail.com>
2022-11-01 10:45:07 -0500

    nvim 0.8

--- init.lua
+++ init.lua
@@ -141,7 +141,7 @@ cmp.setup.cmdline(':', {
 	})
 })
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local on_attach = function(client, bufnr)
 	local bufopts = { noremap=true, silent=true, buffer=bufnr }
@@ -186,8 +186,8 @@ vim.api.nvim_exec([[ let g:go_highlight_operators = 1 ]], false)
 lsp.tsserver.setup{
 	capabilities = capabilities,
 	on_attach = function(client, buffer)
-		client.resolved_capabilities.document_formatting = false
-		client.resolved_capabilities.document_range_formatting = false
+		client.server_capabilities.documentFormattingProvider = false
+		client.server_capabilities.documentRangeFormattingProvider = false
 		on_attach()
 	end,
 	handlers = handlers
